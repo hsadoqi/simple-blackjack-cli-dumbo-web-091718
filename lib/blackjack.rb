bb931872737b91f88d7d297f96777648cda74ae6
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-27 02:29:06 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,7 +1,31 @@
+def welcome
+end
 
+def deal_card
+end
+
+def display_card_total
+end
+
+def prompt_user
+end
+
+def get_user_input
+end
+
+def end_game
+end
+
+def initial_round
+end
+
+def hit?
+end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
+def runner
+end
 
